3528e625aa0e89af5c82c31ea22e9524bd90c2fc
zhangjiaquan92 <zhangjiaquan92@gmail.com>
2022-06-12 23:05:43 -0700

    first try.

---
+++ proj1gold/TestArrayDequeGold.java
@@ -0,0 +1,67 @@
+import org.junit.Test;
+//import org.junit.internal.runners.model.EachTestNotifier;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestArrayDequeGold {
+
+    @Test
+    public void testDeque() {
+        StudentArrayDeque<Integer> studout = new StudentArrayDeque<>();
+        ArrayDequeSolution<Integer> soluout = new ArrayDequeSolution<>();
+        int range = 10;
+        int check = 2;
+        String out = "";
+        for (int i = 0; i < 10; i++) {
+            int randomten = StdRandom.uniform(range);
+            int chk = StdRandom.uniform(check);
+            if (chk == 1) {
+                studout.addFirst(randomten);
+                soluout.addFirst(randomten);
+                Integer actual = studout.get(0);
+                Integer expected = soluout.get(0);
+                out = out + "\naddFirst(" + randomten + ")";
+                assertEquals(out, expected, actual);
+
+
+            } else {
+                studout.addLast(randomten);
+                soluout.addLast(randomten);
+                Integer len = soluout.size() - 1;
+                Integer actual = studout.get(len);
+                Integer expected = soluout.get(len);
+                out = out + "\naddLast(" + randomten + ")";
+                assertEquals(out, expected, actual);
+            }
+
+
+        }
+        for (int i = 0; i < 10; i++) {
+            int chkk = StdRandom.uniform(check);
+            if (chkk == 1) {
+                Integer actual = studout.removeLast();
+                Integer expected = soluout.removeLast();
+                out = out + "\nremoveLast()";
+                assertEquals(out + ", student was " + actual + ", correct was " + expected,
+                        expected, actual);
+            } else {
+                Integer actual = studout.removeFirst();
+                Integer expected = soluout.removeFirst();
+                out = out + "\nremoveFirst()";
+                assertEquals(out + ", student was " + actual + ", correct was " + expected,
+                        expected, actual);
+
+            }
+
+
+        }
+
+
+
+
+
+
+    }
+
+
+}
