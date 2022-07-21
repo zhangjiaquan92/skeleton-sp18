40fea0610f5655550b871374b9d36b2c4dd08635
zhangjiaquan92 <zhangjiaquan92@gmail.com>
2022-07-21 16:21:25 -0700

    working but the edge is not right

--- lab11/lab11/graphs/MazeCycles.java
+++ lab11/lab11/graphs/MazeCycles.java
@@ -9,16 +9,82 @@ public class MazeCycles extends MazeExplorer {
     public int[] edgeTo;
     public boolean[] marked;
     */
+    private boolean targetFound = false;
+
+
 
     public MazeCycles(Maze m) {
         super(m);
+
+
     }
 
     @Override
     public void solve() {
         // TODO: Your code here!
+
+
+
+        // dfs recursion
+
+        edgeTo[0] = 0;
+        dfs_helper(0);
+
+
+
+
+
+
+
+
+
     }
 
     // Helper methods go here
+
+    public void dfs_helper(int v) {
+        marked[v] = true;
+        //announce();
+
+        if (targetFound == true) {
+            return;
+        }
+
+
+        for (int w : maze.adj(v)) {
+            if (!marked[w]) {
+                marked[w] = true;
+                edgeTo[w] = v;
+                //edgeTo[v] = Integer.MAX_VALUE;
+
+
+
+
+                dfs_helper(w);
+                //announce();
+
+            } else if (edgeTo[v] != w){
+                targetFound = true;
+                marked[w] = true;
+                edgeTo[w] = v;
+
+                announce();
+
+                return;
+
+                //edgeTo[v] = Integer.MAX_VALUE;
+
+
+
+            }
+            //edgeTo[v] = Integer.MAX_VALUE;
+
+
+
+        }
+
+
+
+    }
 }
 
