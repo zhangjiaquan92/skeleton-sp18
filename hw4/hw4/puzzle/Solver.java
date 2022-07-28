ea26326feaaa1b90bbaea84ba8156d9784d70d7d
zhangjiaquan92 <zhangjiaquan92@gmail.com>
2022-07-28 01:02:54 -0700

    07282022 daily backup

--- hw4/hw4/puzzle/Solver.java
+++ hw4/hw4/puzzle/Solver.java
@@ -1,35 +1,66 @@
 package hw4.puzzle;
 
 import edu.princeton.cs.algs4.MinPQ;
+import edu.princeton.cs.algs4.Stack;
 import org.w3c.dom.Node;
+
+import java.util.Iterator;
 
 
 public class Solver {
-    int countMove;
-    public class SearchNode {
+    int countMove = 0;
+    SearchNode goal;
+    Stack<WorldState> sol = new Stack<>();
+    public class SearchNode implements Comparable<SearchNode>{
         WorldState curr;
         SearchNode prevNode;
         int moves;
+        int est;
         public SearchNode (WorldState in, int move, SearchNode prev) {
             curr = in;
             moves = move;
             prevNode = prev;
+            est = in.estimatedDistanceToGoal();
 
         }
 
+        @Override
+        public int compareTo(SearchNode another) {
+            Integer CE = (moves + est);
+            Integer AE = (another.est + another.moves);
+            return CE.compareTo(AE);
+        }
     }
-    public void solveHelper(SearchNode) {
-        if
 
-    }
 
     public Solver(WorldState initial) {
-        MinPQ<SearchNode> temp = new MinPQ<>();
-        temp.insert(new SearchNode(initial, 0,  null));
-        SearchNode xx = temp.delMin();
+        MinPQ<SearchNode> PQ = new MinPQ<>();
+        PQ.insert(new SearchNode(initial, 0, null));
 
+        while (!PQ.isEmpty()) {
+            //System.out.println("move count check : " + countMove);
+            SearchNode xx = PQ.delMin();
+            if (xx.curr.isGoal()) {
+                goal = xx;
+                break;
+            }
+            countMove += 1;
+            int s = 0;
+            for (WorldState i : xx.curr.neighbors()) {
 
+                PQ.insert(new SearchNode(i, countMove, xx));
+            }
 
+        }
+
+        int j;
+        for (j = 0; j <= countMove; j += 1) {
+
+            sol.push(goal.curr);
+            goal = goal.prevNode;
+        }
+
+        return;
 
 
 
@@ -38,6 +69,7 @@ public class Solver {
         return countMove;
     }
     public Iterable<WorldState> solution(){
+        return sol;
 
     }
 
