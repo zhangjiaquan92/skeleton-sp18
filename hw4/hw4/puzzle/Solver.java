2702a1982901252eff37e7a2d1957480ba4fb793
zhangjiaquan92 <zhangjiaquan92@gmail.com>
2022-07-28 17:55:25 -0700

    07292022 finish step one

--- hw4/hw4/puzzle/Solver.java
+++ hw4/hw4/puzzle/Solver.java
@@ -4,23 +4,27 @@ import edu.princeton.cs.algs4.MinPQ;
 import edu.princeton.cs.algs4.Stack;
 import org.w3c.dom.Node;
 
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
+import java.util.Objects;
 
 
 public class Solver {
     int countMove = 0;
     SearchNode goal;
+    HashMap<WorldState, Integer> estimate = new HashMap<>();
     Stack<WorldState> sol = new Stack<>();
     public class SearchNode implements Comparable<SearchNode>{
         WorldState curr;
         SearchNode prevNode;
         int moves;
         int est;
-        public SearchNode (WorldState in, int move, SearchNode prev) {
+        public SearchNode (WorldState in, int move, SearchNode prev, int estimate) {
             curr = in;
             moves = move;
             prevNode = prev;
-            est = in.estimatedDistanceToGoal();
+            est = estimate;
 
         }
 
@@ -30,25 +34,46 @@ public class Solver {
             Integer AE = (another.est + another.moves);
             return CE.compareTo(AE);
         }
+        public boolean parentCheck() {
+            return ((Word)curr).equals((prevNode.curr));
+
+            //return curr.equals(prevNode.curr);
+        }
     }
 
 
     public Solver(WorldState initial) {
         MinPQ<SearchNode> PQ = new MinPQ<>();
-        PQ.insert(new SearchNode(initial, 0, null));
+        int dist = initial.estimatedDistanceToGoal();
+        estimate.put(initial, dist);
+        PQ.insert(new SearchNode(initial, 0, null, dist));
 
         while (!PQ.isEmpty()) {
             //System.out.println("move count check : " + countMove);
             SearchNode xx = PQ.delMin();
             if (xx.curr.isGoal()) {
                 goal = xx;
+                countMove = xx.moves;
                 break;
             }
-            countMove += 1;
-            int s = 0;
+            countMove = 1 + xx.moves;
             for (WorldState i : xx.curr.neighbors()) {
+                int jj;
+                if (estimate.containsKey(i)) {
+                    jj = estimate.get(i);
+                }else {
+                    jj = i.estimatedDistanceToGoal();
+                    estimate.put(i, jj);
+                }
 
-                PQ.insert(new SearchNode(i, countMove, xx));
+                SearchNode temp = new SearchNode(i, countMove, xx, jj);
+                //System.out.println("parent check output : " + temp.parentCheck());
+                if (!Objects.nonNull(xx.prevNode)) {  // https://docs.oracle.com/javase/8/docs/api/java/util/Objects.html#nonNull-java.lang.Object-
+                    PQ.insert(temp);
+                }else if (!i.equals(xx.prevNode.curr)) {
+                    PQ.insert(temp);
+                }
+
             }
 
         }
